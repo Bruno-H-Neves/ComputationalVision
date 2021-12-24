feb375f04d9650a08766f2155a940e84bd4613dd
Bruno-H-Neves <bn.prog.home@gmail.com>
2021-12-24 00:55:47 +0000

    version 1.1

--- OpenCVTutorial.py
+++ OpenCVTutorial.py
@@ -1,4 +1,14 @@
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def exit_image(title_win,img):
+    while True:
+        cv2.imshow(title_win, img)
+        key = cv2.waitKey(1)    
+        if key == 27 or key==ord('q'): 
+            break
+
 
 imageTypes={'Type1':[1080,1920],
             'Type2': [1080,1080],
@@ -7,16 +17,12 @@ imageTypes={'Type1':[1080,1920],
             'Type5': [1200,630],
             'Type6': [851,315],
             'Type7': [640,480]}
-            
+
 cap = cv2.VideoCapture(0)
-
 #settings Camera
-
 cap.set(3,imageTypes['Type2'][0])  # channel 3: windows width
 cap.set(4,imageTypes['Type2'][1])  # channel 4: windows height
 cap.set(10,100)                    # channel 10: Brightness of the image
-
-
 if cap.isOpened():
     CtrlRead, frame = cap.read()
     while True:
@@ -25,12 +31,45 @@ if cap.isOpened():
         key = cv2.waitKey(1)    
         if key == 27 or key==ord('q'): 
             break
-
 cv2.imwrite("LastFrame.png", frame)   #Save last frame current directory
-
-
 cap.release()
 cv2.destroyAllWindows()
+
+print("Image Operation")
+kernel=np.ones((5,5),np.uint8) #one's matrix for using in morphologic operations
+img = cv2.imread("LastFrame.png",1)
+if np.shape(img) ==():
+    print("Invalid Image")
+title_win='Original Image  -> Press "ESC"  or "q" for exit'
+exit_image(title_win,img)
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  #RGB to Gray scale 
+title_win='Gray Image  -> Press "ESC"  or "q" for exit'
+exit_image(title_win,imgGray)
+imgBlur_rgb = cv2.GaussianBlur(img,(7,7),0)     #Gaussian Filter
+title_win='Gaussian Filter RGB Image  -> Press "ESC"  or "q" for exit'
+exit_image(title_win,imgBlur_rgb)
+imgBlur_gray = cv2.GaussianBlur(imgGray,(7,7),0, cv2.BORDER_TRANSPARENT)
+title_win='Gaussian Filter Gray Scale Image  -> Press "ESC"  or "q" for exit'
+exit_image(title_win,imgBlur_gray)
+imgCanny = cv2.Canny(img, 150, 200) 
+title_win='Edge Image -> Press "ESC"  or "q" for exit'
+exit_image(title_win,imgCanny)
+imgDialation = cv2.dilate(imgCanny, kernel, iterations=1) 
+title_win='Dilate Edge Image -> Press "ESC"  or "q" for exit'
+exit_image(title_win,imgDialation)
+imgEroded = cv2.erode(imgDialation, kernel, iterations=1)
+title_win='Erode Dilated Image -> Press "ESC"  or "q" for exit'
+exit_image(title_win,imgEroded)
+gray_eq = cv2.equalizeHist(imgGray)
+title_win='Gray Image Equalize -> Press "ESC"  or "q" for exit'
+exit_image(title_win,gray_eq)
+
+
+cv2.destroyAllWindows()
+
+
+
 
 
 
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ Tutorial OpenCV
 version 0: Read webcam
 version 0.1: Save last frame
 version 0.2: Set windows parameters
+version 1.1: image operation: RGB to GRAY scale, Gaussian filter, edge, morphologic, equalized
