0a2eb016cdd8ec7ebe3fa8744bd4e3e4fe7f5317
Bruno-H-Neves <bn.prog.home@gmail.com>
2021-12-27 18:40:28 +0000

    V0.3: Image Subtractor settings image and image show

--- OpenCvImgSubtractor.py
+++ OpenCvImgSubtractor.py
@@ -66,3 +66,19 @@ def subtractor(img1,img2,thr=150):
     ImgSub =(ImgSub<thr)*ImgSub
     return ImgSub
 
+#Types of images
+imageTypes={'Type1':[1080,1920],
+            'Type2': [1080,1080],
+            'Type3': [1080,566],
+            'Type4': [1080,1350],
+            'Type5': [1200,630],
+            'Type6': [851,315],
+            'Type7': [640,480]}
+
+#settings show Camera
+cap = cv2.VideoCapture(0)
+cap.set(3,imageTypes['Type2'][0])  # channel 3: windows width
+cap.set(4,imageTypes['Type2'][1])  # channel 4: windows height
+cap.set(10,100)                    # channel 10: Brightness of the image
+
+
