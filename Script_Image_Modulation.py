e54ee19ac69576fd3977f9e52ac31c2ec03471c4
Bruno-H-Neves <bn.prog.home@gmail.com>
2021-12-28 10:00:33 +0000

    Version 1: define windows

--- Script_Image_Modulation.py
+++ Script_Image_Modulation.py
@@ -2,7 +2,8 @@ import PySimpleGUI as sg
 import cv2 as cv
 import numpy as np
 
-
+def FrameTitle(img,Title):
+    return  cv.putText(img,Title,(1,10),cv.FONT_HERSHEY_COMPLEX,0.45,(255,255,255),1)
 
 
 def main():
@@ -14,35 +15,47 @@ def main():
             'Type6': [851,315],
             'Type7': [640,480]}
 
-    filename = sg.popup_get_file('Search File')
+    filename = sg.popup_get_file('Search File   Cancel for  WebCam')
     if filename is None:
         vidFile = cv.VideoCapture(0)
+        num_frames=0
     else:
         vidFile = cv.VideoCapture(filename)
-    num_frames = vidFile.get(cv.CAP_PROP_FRAME_COUNT)
-    fps = vidFile.get(cv.CAP_PROP_FPS)
+        num_frames = vidFile.get(cv.CAP_PROP_FRAME_COUNT)
+        fps = vidFile.get(cv.CAP_PROP_FPS)
 
     sg.theme('Black')
 
-    layout = [[sg.Slider(range=(0, num_frames),size=(60, 10), orientation='h', key='-slider-'),sg.Button('Exit', size=(7, 1), font='Helvetica 14')],
-              [sg.Image(filename='',size=(100,10), key='-image1-'), sg.Image(filename='',size=(100,10), key='-image2-')],
-              [sg.Image(filename='',size=(50,10), key='-image3-'), sg.Image(filename='',size=(50,10), key='-image4-')]]
+    layout = [[ sg.Slider(range=(0, num_frames),size=(53, 10), orientation='h', key='-slider-'),
+                sg.Button('Exit', size=(7, 1), font='Helvetica 14')],
+              [ sg.Image(filename='', key='-image1-'),
+                sg.Image(filename='', key='-image2-')],
+              [ sg.Image(filename='', key='-image3-'), 
+                sg.Image(filename='',key='-image4-'),sg.Image(filename='', key='-image5-')]]
 
 
-    window = sg.Window('Frame', layout, no_titlebar=False, location=(0, 0),size=(1920,1080))
+    window = sg.Window('Frame', layout, no_titlebar=True, location=(40,40),size=(1080,600))
 
     image_elem1 = window['-image1-']
     image_elem2 = window['-image2-']
     image_elem3 = window['-image3-']
     image_elem4 = window['-image4-']
+    image_elem5 = window['-image5-']
     slider_elem = window['-slider-']
 
+    ret, frame = vidFile.read()
+    print(frame.shape)
+    scale=frame.shape[0]/frame.shape[1]
+    nb=280
+    frame = cv.resize(frame,(nb,int(nb*scale))) 
+    nb2=nb*1.5
     cur_frame = 0
     while vidFile.isOpened():
         event, values = window.read(timeout=0)
         if event in ('Exit', None):
             break
         ret, frame = vidFile.read()
+        frame = cv.resize(frame,(nb,int(nb*scale))) 
         if not ret:  
             break
         if int(values['-slider-']) != cur_frame-1:
@@ -50,18 +63,25 @@ def main():
             vidFile.set(cv.CAP_PROP_POS_FRAMES, cur_frame)
         slider_elem.update(cur_frame)
         cur_frame += 1
-        frameResize = cv.resize(frame,(imageTypes['Type6'][0],imageTypes['Type6'][1]))
-        imgTransform = cv.cvtColor(frameResize, cv.COLOR_RGB2GRAY)
+        frameRGB=frame.copy()
+        frameRGB_Tl = FrameTitle(frameRGB,'RGB')
+        frameGray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
         kernel = np.ones((5,5),np.float32)/25
-        imgTransform2D = cv.filter2D(imgTransform,-1,kernel) 
-        imgTransformLap = cv.Canny(imgTransform2D,100,200)
-        imgbytes = cv.imencode('.png', frameResize)[1].tobytes()  
-        imgbytes2 = cv.imencode('.png', imgTransform)[1].tobytes() 
-        imgbytes3 = cv.imencode('.png', imgTransform2D)[1].tobytes() 
-        imgbytes4 = cv.imencode('.png', imgTransformLap)[1].tobytes() 
+        frameLBF = cv.filter2D(frameGray,-1,kernel)
+        frameHBF = cv.Canny(frameGray,100,200) 
+        frameGray_Tl = FrameTitle(frameGray,'Gray')
+        frameLBF_Tl = FrameTitle( frameLBF,'LBF')
+        frameHBF_Tl = FrameTitle(frameHBF,'HBF')
+        imgbytes = cv.imencode('.png', frameRGB_Tl)[1].tobytes()  
+        imgbytes2 = cv.imencode('.png', frameGray_Tl)[1].tobytes() 
+        imgbytes3 = cv.imencode('.png', frameLBF_Tl)[1].tobytes() 
+        imgbytes4 = cv.imencode('.png', frameHBF_Tl)[1].tobytes() 
+        imgbytes5 = cv.imencode('.png', cv.resize(frameRGB_Tl,(int(nb2),int(nb2*scale))))[1].tobytes() 
+        
         image_elem1.update(data=imgbytes)
         image_elem2.update(data=imgbytes2)
         image_elem3.update(data=imgbytes3)
-        image_elem4.update(data=imgbytes4)        
+        image_elem4.update(data=imgbytes4)   
+        image_elem5.update(data=imgbytes5)      
 
 main()
